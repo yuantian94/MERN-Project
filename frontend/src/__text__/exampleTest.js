126713bd637fc4c2d5ceb4439ea450d49e78c0fe
Yuan Tian <michealty94@gmail.com>
2023-06-21 09:56:01 -0500

    Delete frontend/src/__text__ directory

--- frontend/src/__text__/exampleTest.js
+++
@@ -1,9 +0,0 @@
-const sum = require("../utils/exampleModule")
-
-test("two plus four is six", () => {
-    expect(2 + 2).toBe(4);
-})
-
-test('two plus four is six', () => {
-  expect(sum(2,4)).toBe(6);
-});
